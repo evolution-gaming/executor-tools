e955e0381e8acca0263e81f0b5da587b813895d7
Yaroslav Klymko <t3hnar@gmail.com>
2018-08-14 15:33:56 +0200

    make CurrentThreadExecutionContext a case object

--- src/main/scala/com/evolutiongaming/concurrent/CurrentThreadExecutionContext.scala
+++ src/main/scala/com/evolutiongaming/concurrent/CurrentThreadExecutionContext.scala
@@ -2,7 +2,7 @@ package com.evolutiongaming.concurrent
 
 import scala.concurrent.ExecutionContextExecutor
 
-object CurrentThreadExecutionContext extends ExecutionContextExecutor {
+case object CurrentThreadExecutionContext extends ExecutionContextExecutor {
   def execute(runnable: Runnable) = runnable.run()
   def reportFailure(cause: Throwable) = throw cause
 }
