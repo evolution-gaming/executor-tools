7ba8156b7ca80acdfaf804f76f30fe2c78479135
Z1kkurat <kshelopugin@gmail.com>
2022-11-17 11:19:22 +0100

    Cross-compile to Scala 3.2.1

---
+++ src/main/scala-3/com/evolutiongaming/concurrent/CurrentThreadExecutionContext.scala
@@ -0,0 +1,10 @@
+package com.evolutiongaming.concurrent
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+
+case object CurrentThreadExecutionContext extends ExecutionContextExecutor {
+  private val ec = ExecutionContext.parasitic
+
+  def execute(runnable: Runnable): Unit     = ec.execute(runnable)
+  def reportFailure(cause: Throwable): Unit = ec.reportFailure(cause)
+}
